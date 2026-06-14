26bd3ba4b4c1ce7beebb1975da7217ac867428b7
karthick-vm <karthickvm1612@gmail.com>
2026-06-15 02:53:23 +0530

    Add Docker PostgreSQL and Redis support

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
 
+    REDIS_HOST: str
+    REDIS_PORT: int
+
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int  
